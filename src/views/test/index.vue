49834f9c7ad949ba606ddc2e0381d875071bcbd0
wupeng163 <wupenging163@163.com>
2022-03-24 11:54:36 +0800

    udpate

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,52 +1,54 @@
 <template>
-  <div class="px-12 w-full h-full flex flex-col items-center">
-    <!-- <div class="flex justify-center items-center">
+  <main-card>
+    <div class="px-12 w-full h-full flex flex-col items-center">
+      <!-- <div class="flex justify-center items-center">
       <change-theme />
       <span>配置主题</span>
-    </div>-->
-    <div class="flex flex-wrap gap-12 mt-12">
-      <el-button @click="toast">El Message</el-button>
-      <el-button type="primary" @click="count++">count is: {{ count }}</el-button>
-      <el-button type="success" @click="count++">count is: {{ count }}</el-button>
-      <el-button type="warning" @click="count++">count is: {{ count }}</el-button>
-      <el-button type="danger" @click="count++">count is: {{ count }}</el-button>
-      <el-button type="info" @click="count++">count is: {{ count }}</el-button>
-    </div>
-    <div>
-      <el-tag
-        v-for="tag in tags"
-        :key="tag.name"
-        class="mx-1"
-        closable
-        :type="tag.type"
-      >{{ tag.name }}</el-tag>
-    </div>
-    <el-button>
-      <template #icon>
-        <app-svg-icon icon-name="fn-shezhiq" class="w-20 h-20"></app-svg-icon>
-      </template>
-      <span>{{ '配置' }}</span>
-    </el-button>
-
-    <div class="flex-1 w-full mt-12 flex flex-col items-center">
-      <el-button @click="handlerOpen">打开弹框</el-button>
-      <div class>
-        <el-link href="https://element.eleme.io" target="_blank">default</el-link>
-        <el-link type="primary">primary</el-link>
-        <el-link type="success">success</el-link>
-        <el-link type="warning">warning</el-link>
-        <el-link type="danger">danger</el-link>
-        <el-link type="info">info</el-link>
+      </div>-->
+      <div class="flex flex-wrap gap-12 mt-12">
+        <el-button @click="toast">El Message</el-button>
+        <el-button type="primary" @click="count++">count is: {{ count }}</el-button>
+        <el-button type="success" @click="count++">count is: {{ count }}</el-button>
+        <el-button type="warning" @click="count++">count is: {{ count }}</el-button>
+        <el-button type="danger" @click="count++">count is: {{ count }}</el-button>
+        <el-button type="info" @click="count++">count is: {{ count }}</el-button>
       </div>
-
       <div>
-        <a href="https://www.baidu.com/s?wd=vue3">asdfasdfa</a>asdfasdfa
+        <el-tag
+          v-for="tag in tags"
+          :key="tag.name"
+          class="mx-1"
+          closable
+          :type="tag.type"
+        >{{ tag.name }}</el-tag>
       </div>
-      <div class="h-100 w-full bg-yellow-300" v-loading="true"></div>
-    </div>
+      <el-button>
+        <template #icon>
+          <app-svg-icon icon-name="fn-shezhiq" class="w-20 h-20"></app-svg-icon>
+        </template>
+        <span>{{ '配置' }}</span>
+      </el-button>
 
-    <DetialDialog v-if="visible" v-model:visible="visible"></DetialDialog>
-  </div>
+      <div class="flex-1 w-full mt-12 flex flex-col items-center">
+        <el-button @click="handlerOpen">打开弹框</el-button>
+        <div class>
+          <el-link href="https://element.eleme.io" target="_blank">default</el-link>
+          <el-link type="primary">primary</el-link>
+          <el-link type="success">success</el-link>
+          <el-link type="warning">warning</el-link>
+          <el-link type="danger">danger</el-link>
+          <el-link type="info">info</el-link>
+        </div>
+
+        <div>
+          <a href="https://www.baidu.com/s?wd=vue3">asdfasdfa</a>asdfasdfa
+        </div>
+        <div class="h-100 w-full bg-yellow-300" v-loading="true"></div>
+      </div>
+
+      <DetialDialog v-if="visible" v-model:visible="visible"></DetialDialog>
+    </div>
+  </main-card>
 </template>
 
 <script lang="ts">
--- src/views/testTwo/index.vue
+++ src/views/testTwo/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="overflow-auto">
-    <div class="h-500" v-for="item in 1" :key="item">
-      asdf
-      <el-button>
-        <AppSvgIcon iconName="fn-pen" class="w-20 h-20 aa" />asdf
-      </el-button>
-      <app-svg-icon iconName="icon-edit" />
+  <main-card>
+    <div class="overflow-auto">
+      <div class="h-500" v-for="item in 1" :key="item">
+        asdf
+        <el-button>
+          <AppSvgIcon iconName="fn-pen" class="w-20 h-20 aa" />asdf
+        </el-button>
+        <app-svg-icon iconName="icon-edit" />
+      </div>
     </div>
-  </div>
+  </main-card>
 </template>
 
 <script lang="ts">

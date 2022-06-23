cc0264d65bdc4f97e19599c604384cae95d916e3
wupeng163 <wupenging163@163.com>
2022-06-23 16:56:57 +0800

    feat:add vueuse

--- src/components/customForm/components/customRadio.vue
+++ src/components/customForm/components/customRadio.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    单选框
-  </div>
+  <el-radio-group v-model="val" class="ml-4">
+    <el-radio label="1" size="large">Option 1</el-radio>
+    <el-radio label="2" size="large">Option 2</el-radio>
+  </el-radio-group>
 </template>
 
 <script setup lang="ts">
--- src/utils/vueUse/index.ts
+++ src/utils/vueUse/index.ts
@@ -1,5 +1,6 @@
-import { reactive, ref } from "vue";
+import { reactive, ref, computed, PropType, defineProps, defineEmits } from "vue";
 
+// use分页
 export function useElPagination() {
   const pagination = reactive({
     pageSizes: [10, 20, 30, 50, 100, 150, 200, 300],
@@ -8,4 +9,31 @@ export function useElPagination() {
     total: 0
   })
   return pagination
+}
+// useProps
+export function useDefaultProps<T extends Record<keyof T, any>>(arg?: T) {
+  return {
+    modelValue: {
+      type: String as PropType<any>
+    },
+    ...arg
+  }
+}
+// useEmits
+export function useDefaultEmits() {
+  return ['update:modelValue']
+}
+// useModelValue
+export function useModelVal() {
+  const props = defineProps(useDefaultProps())
+  const emits = defineEmits(useDefaultEmits())
+  const val = computed({
+    get() {
+      return props.modelValue
+    },
+    set(v) {
+      emits('update:modelValue', v)
+    }
+  })
+  return val
 }

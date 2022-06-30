7397d1522195cbaf50dff392a61f7e2bc6b1adb1
wupeng163 <wupenging163@163.com>
2022-06-30 10:29:18 +0800

    feat:form配置文件的分离

--- src/components/FormCustom/index.vue
+++ src/components/FormCustom/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-form v-bind="$attrs" ref="ruleFormRef" v-model="formDate" :rules="props.rules">
+    <el-form v-bind="$attrs" ref="ruleFormRef" :model="formDate" :rules="props.rules">
       <el-row>
         <el-col v-for="(item, index) in formColumn" :key="index + 'sd'" v-bind="item.col">
           <template v-if="item.slotName">
@@ -20,7 +20,7 @@
 </template>
 
 <script setup lang="ts">
-import { useSlots, reactive, ref, useAttrs } from 'vue'
+import { useSlots, reactive, ref, useAttrs, watch, Ref, markRaw } from 'vue'
 import { defaultProps } from './props'
 import { useModelVal, useDefaultProps, useDefaultEmits } from '@/utils/vueUse';
 import type { FormInstance } from 'element-plus'
@@ -30,7 +30,7 @@ const emits = defineEmits([...useDefaultEmits()])
 const formDate = useModelVal(props, emits)
 
 // 将表单绑定的ref暴露给父组件
-const ruleFormRef = ref<FormInstance>();
+const ruleFormRef = ref();
 defineExpose({ ruleFormRef });
 
 </script>
--- src/views/drawLine/index.vue
+++ src/views/drawLine/index.vue
@@ -2,7 +2,7 @@
   <main-card>
     <span>form表单测试</span>
     <section class="w-500">
-      <form-custom label-width="80px" v-model="state.formDate" :form-column="formColumn" :rules="rules">
+      <form-custom label-width="80px" v-model="state.formDate" :form-column="column" :rules="rules">
         <template #footer>
           <div class="w-full flex justify-end">
             <el-button>取消</el-button>
@@ -16,85 +16,13 @@
 
 <script setup lang="ts">
 import { ref, reactive, markRaw } from 'vue'
-import CustomSelect from '@/components/customSelect/index.vue'
-const options = [
-  {
-    value: 'zhangan',
-    label: 'shangsan'
-  }
-]
-const formColumn = markRaw([
-  {
-    col: { span: 11 },
-    formItemProps: {},
-    label: '下拉框',
-    prop: 'select',
-    componentsProps: { options: options, clearable: true, onChange: handlerChange, onVisibleChange: handlerChangeVisible },
-    type: CustomSelect
-  },
-  {
-    col: { span: 11, offset: 2 },
-    formItemProps: {},
-    label: 'name',
-    prop: 'val',
-    componentsProps: { clearable: true },
-    type: 'el-input'
-  },
-  {
-    col: { span: 11, },
-    formItemProps: {},
-    label: '数字:',
-    prop: 'val2',
-    componentsProps: { clearable: true, placeholder: "Please input" },
-    type: 'el-input-number'
-  },
-  {
-    col: { span: 11, offset: 2 },
-    formItemProps: {},
-    label: '评分',
-    prop: 'val3',
-    componentsProps: { clearable: true },
-    type: 'el-rate'
-  },
-  {
-    col: { span: 11, },
-    formItemProps: {},
-    label: '滑块',
-    prop: 'val4',
-    componentsProps: { clearable: true },
-    type: 'el-slider'
-  },
-  {
-    col: { span: 24, },
-    formItemProps: {},
-    label: '意见',
-    prop: 'val5',
-    componentsProps: { clearable: true, type: "textarea", placeholder: '请输入你的意见或者建议' },
-    type: 'el-input'
-  },
-  {
-    slotName: 'footer',
-    col: { span: 24 },
-    formItemProps: {},
-    label: '',
-    prop: '',
-    componentsProps: {},
-    type: ''
-  }
-])
-const rules = markRaw({
-  select: [
-    { required: true, message: 'Please select name', trigger: 'change' }
-  ],
-  val: [
-    { required: true, message: 'Please input Activity name', trigger: 'blur' }
-  ],
-  val5: [
-    {
-      required: true, message: 'Please input your suggestion', trigger: 'blur'
-    }
-  ]
+import { useFormColumn } from './index'
+const { column, emitter, rules, } = useFormColumn()
+
+emitter.on('handlerChange', (val) => {
+  console.log(val, "val")
 })
+
 const state = reactive({
   formDate: {
     select: 'zhangan',
@@ -106,13 +34,7 @@ const state = reactive({
   },
 })
 
-function handlerChange(val: any) {
-  console.log(val)
-}
 
-function handlerChangeVisible(val: boolean) {
-  console.log(val)
-}
 
 function handleSubmit() {
   console.log(state.formDate, "formDate")
